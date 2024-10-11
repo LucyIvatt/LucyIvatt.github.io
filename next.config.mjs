932f63e7100c0d13a2857a156667bb77f5d5891a
Lucy Ivatt <lucy.ivatt@xdesign.com>
2024-10-11 16:03:39 +0100

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: `/${process.env.REPO_NAME}`,
+  output: "export",
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
+
+module.exports = nextConfig;
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 bg-gradient-to-t from-gray-700 via-rose-500 to-orange-400">
+      <main className="flex flex-col gap-8 items-center bg-white p-8 rounded-xl w-full max-w-[700px] shadow-md">
+        <h1 className="text-4xl text-black text-center">
+          👷‍♀️ This website is under construction 👷‍♀️
+        </h1>
+
+        <p className="text-black text-wrap text-center">
+          I am currently in the process of rebuilding my portfolio website with
+          React & Tailwind! When I have finished my first version it will be
+          released here.
+        </p>
+        <p className="text-black text-wrap text-center">
+          Many thanks,
+          <br />
+          Lucy ❤️
+        </p>
+      </main>
+    </div>
+  );
+}
