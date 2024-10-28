87a94f1c5baa40960370a3f1d13a2155b11a8351
Lucy Ivatt <lucy.ivatt@xdesign.com>
2024-10-28 22:39:49 +0000

    refactored global styles, removed hardcoded background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Navigation } from "@/components/Navigation/Navigation";
 
 export default function Home() {
   return (
-    <div className="bg-background">
+    <div>
       <Navigation />
       <Hero />
     </div>
